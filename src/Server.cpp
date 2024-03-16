100515f4ad192fdc338d74d9ce3377fe753b7aee
martinx0520 <martinx0520@gmail.com>
2024-03-16 00:03:22 -0400

    Add custom port support

--- src/Server.cpp
+++ src/Server.cpp
@@ -71,7 +71,6 @@ void parse_Array(char *msg, int client_fd) {
         mp.erase(parsed_Arr[4]);
         return_msg = (char *)"$-1\r\n";
       } else {
-        std::cout << "key unexpired" << std::endl;
         auto e = mp[parsed_Arr[4]];
         std::string response = e.length + delim + e.value + delim;
         return_msg = (char *)response.c_str();
@@ -126,7 +125,7 @@ void request_handler(int client_fd) {
   close(client_fd);
 }
 
-int main() {
+int main(int argc, char *argv[]) {
 
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
@@ -143,14 +142,19 @@ int main() {
     return 1;
   }
 
+  int hostPort = 6379;
+  if (argc > 2 && std::string(argv[1]) == "--port") {
+    hostPort = std::stoi(argv[2]);
+  }
+
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(6379);
+  server_addr.sin_port = htons(hostPort);
 
   if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) !=
       0) {
-    std::cerr << "Failed to bind to port 6379\n";
+    std::cerr << "Failed to bind to port " << hostPort << "\n";
     return 1;
   }
 

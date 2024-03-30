abef27229cb04855be8cbdcc0a1fc7ce9bdd6067
martinx0520 <121643655+martinxing0520@users.noreply.github.com>
2024-03-30 15:10:54 -0400

    Added support for handling replconf

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -25,18 +25,21 @@ const char COLON_SIGN = ':';
 
 const std::string delim = "\r\n";
 
-long long get_time() {
+long long get_time()
+{
   using namespace std::chrono;
   return duration_cast<milliseconds>(system_clock::now().time_since_epoch())
       .count();
 }
 
-void parse_Array(char *msg, CommandLineEntry &cliEntry, int client_fd) {
+void parse_Array(char *msg, CommandLineEntry &cliEntry, int client_fd)
+{
   std::string command_Str(msg), word;
   std::vector<std::string> parsed_Arr;
 
   size_t pos_st = 0, pos_ed, delim_len = delim.length();
-  while ((pos_ed = command_Str.find(delim, pos_st)) != std::string::npos) {
+  while ((pos_ed = command_Str.find(delim, pos_st)) != std::string::npos)
+  {
     word = command_Str.substr(pos_st, pos_ed - pos_st);
     pos_st = pos_ed + delim_len;
 
@@ -46,47 +49,73 @@ void parse_Array(char *msg, CommandLineEntry &cliEntry, int client_fd) {
 
   int arrLen = parsed_Arr.size();
   std::string return_msg{};
-  if (parsed_Arr[2] == "ping") {
+  if (parsed_Arr[2] == "ping")
+  {
     return_msg = "+PONG\r\n";
-  } else if (parsed_Arr[2] == "echo") {
+  }
+  else if (parsed_Arr[2] == "echo")
+  {
     return_msg = parsed_Arr[3] + delim + parsed_Arr[4] + delim;
-  } else if (parsed_Arr[2] == "set") {
+  }
+  else if (parsed_Arr[2] == "set")
+  {
     cliEntry.mp[parsed_Arr[4]] = {parsed_Arr[5], parsed_Arr[6]};
-    if (arrLen > 7 && parsed_Arr[8] == "px") {
+    if (arrLen > 7 && parsed_Arr[8] == "px")
+    {
       cliEntry.mp[parsed_Arr[4]].expiry =
           get_time() + std::stoll(parsed_Arr[10]);
     }
     return_msg = "+OK\r\n";
-  } else if (parsed_Arr[2] == "get") {
-    if (cliEntry.mp.count(parsed_Arr[4])) {
+  }
+  else if (parsed_Arr[2] == "get")
+  {
+    if (cliEntry.mp.count(parsed_Arr[4]))
+    {
       if (cliEntry.mp[parsed_Arr[4]].expiry &&
-          get_time() >= cliEntry.mp[parsed_Arr[4]].expiry) {
+          get_time() >= cliEntry.mp[parsed_Arr[4]].expiry)
+      {
         cliEntry.mp.erase(parsed_Arr[4]);
         return_msg = "$-1\r\n";
-      } else {
+      }
+      else
+      {
         auto e = cliEntry.mp[parsed_Arr[4]];
         return_msg = e.length + delim + e.value + delim;
       }
-    } else {
+    }
+    else
+    {
       return_msg = "$-1\r\n";
     }
-  } else if (parsed_Arr[2] == "info") {
+  }
+  else if (parsed_Arr[2] == "info")
+  {
     std::vector<std::pair<std::string, std::string>> fields;
-    if (parsed_Arr[4] == "replication") {
+    if (parsed_Arr[4] == "replication")
+    {
       fields.push_back({"role", cliEntry.role});
       fields.push_back({"master_replid", cliEntry.master_replid});
       fields.push_back(
           {"master_repl_offset", std::to_string(cliEntry.master_repl_offset)});
       int len = 0;
-      for (auto p : fields) {
+      for (auto p : fields)
+      {
         len += p.first.length() + p.second.length() + 3;
         return_msg += p.first + ":" + p.second + delim;
       }
       return_msg = "$" + std::to_string(len - 2) + delim + return_msg;
-    } else {
+    }
+    else
+    {
       return_msg = "+\r\n";
     }
-  } else {
+  }
+  else if (parsed_Arr[2] == "replconf")
+  {
+    return_msg = "+OK\r\n";
+  }
+  else
+  {
     return_msg = "+\r\n";
   }
 
@@ -97,9 +126,11 @@ void parse_Array(char *msg, CommandLineEntry &cliEntry, int client_fd) {
             << std::endl;
 }
 
-void parse_msg(char *msg, CommandLineEntry &cliEntry, int client_fd) {
+void parse_msg(char *msg, CommandLineEntry &cliEntry, int client_fd)
+{
   char fb = msg[0];
-  switch (fb) {
+  switch (fb)
+  {
   case DOLLAR_SIGN:
     return;
   case ASTERISK_SIGN:
